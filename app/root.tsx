acf1cb3df77c6aa00ab6a06d97a64c54c7f7aeaa
Corey Jepperson <corey@entropy.cc>
2022-12-27 12:12:59 -0600

    pwa

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useLocation, useMatches } from '@remix-run/react';
 import type { LoaderFunction, MetaFunction } from "@remix-run/cloudflare";
 import type { LinksFunction } from "@remix-run/cloudflare";
 import {
@@ -10,34 +12,67 @@ import {
 } from "@remix-run/react";
 import styles from "./tailwind.css";
 import { Button } from "react-daisyui";
+let isMount = true;
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
-
 export let loader: LoaderFunction = async ({ context }) => {
-  // Use context.YOUR_ENVIRONMENT_VARIABLE to access your environment variable.
-  console.log("context", context);
-  return {};
+  // Use context.YOUR_ENVIRONMENT_VARIABLE to access your environment variable. console.log("context", context); return {};
 };
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
-
 export default function App() {
+  let location = useLocation();
+  let matches = useMatches();
+
+  React.useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ("serviceWorker" in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: "REMIX_NAVIGATION",
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: "REMIX_NAVIGATION",
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener("controllerchange", listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener(
+            "controllerchange",
+            listener
+          );
+        };
+      }
+    }
+  }, [location]);
+
   return (
     <html lang="en">
+       
       <head>
-        <Meta />
-        <Links />
-      </head>
+         
+        <Meta /> <Links /> 
+      </head> 
       <body>
-        <h4 className="mb-4">Current Theme: </h4>
-        <Button color="primary">Click me!</Button>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
+         
+        <h4 className="mb-4">Current Theme: </h4> 
+        <Button color="primary">Click me!</Button> <Outlet /> 
+        <ScrollRestoration /> <Scripts /> <LiveReload /> 
+      </body> 
     </html>
   );
 }
