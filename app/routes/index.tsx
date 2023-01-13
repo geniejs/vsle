4ec560ecfa3740e334478d1e986a873e19f1b5ba
Corey Jepperson <corey@entropy.cc>
2023-01-13 15:39:38 -0600

    home improve

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,8 @@
 import type { LoaderFunction } from "@remix-run/cloudflare";
 import { useFetcher, useLoaderData } from "@remix-run/react";
+import Quintle from "~/components/quintle/quintile";
 import { getAuthenticator } from "~/services/auth.server";
-
+import { Button } from "react-daisyui";
 export let loader: LoaderFunction = async ({ request, context }) => {
   const user = await getAuthenticator(
     context.env as Record<string, string>
@@ -20,29 +21,30 @@ export default function Index() {
           <h1 className="text-center text-4xl">{user.name}</h1>
           <fetcher.Form method="post" action="/auth/signout/google">
             <p>
-              <button
+              <Button
                 color="primary"
                 type="submit"
                 disabled={fetcher.state === "submitting"}
               >
                 Sign Out
-              </button>
+              </Button>
             </p>
           </fetcher.Form>
         </div>
       ) : (
         <fetcher.Form method="post" action="/auth/signin/google">
           <p>
-            <button
+            <Button
               color="primary"
               type="submit"
               disabled={fetcher.state === "submitting"}
             >
               Sign In
-            </button>
+            </Button>
           </p>
         </fetcher.Form>
       )}
+      <Quintle></Quintle>
     </div>
   );
 }
